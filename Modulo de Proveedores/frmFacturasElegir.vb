35b78675a56adce3108ab18d0ac1901f6d346a71
Szunigau <selvinulin99@gmail.com>
2018-05-09 16:23:31 -0600

    Selvin Complete

--- Modulo de Proveedores/frmFacturasElegir.vb
+++ Modulo de Proveedores/frmFacturasElegir.vb
@@ -76,7 +76,7 @@ Public Class frmFacturasElegir
             Dim entradas As List(Of GridViewRowInfo) = (From x In grdFacturas.Rows Where CBool(x.Cells("chmElegir").Value) Select x).ToList
 
             For Each entrada As GridViewRowInfo In entradas
-                listaEntradas.Add(New Tuple(Of Integer, String, Decimal)(CInt(entrada.Cells("id").Value), CStr2(entrada.Cells("Factura").Value), CDec(entrada.Cells("chmMontoPago").Value)))
+                listaEntradas.Add(New Tuple(Of Integer, String, Decimal)(CInt(entrada.Cells("id").Value), CStr2(entrada.Cells("Factura").Value), CDec(entrada.Cells("txmMontoPagar").Value)))
             Next
             mdlPublicVars.superSearchLista3 = listaEntradas
         ElseIf idcliente > 0 Then
@@ -85,7 +85,7 @@ Public Class frmFacturasElegir
             Dim salidas As List(Of GridViewRowInfo) = (From x In grdFacturas.Rows Where CBool(x.Cells("chmElegir").Value) Select x).ToList
 
             For Each salida As GridViewRowInfo In salidas
-                listaSalidas.Add(New Tuple(Of Integer, String, Decimal)(CInt(salida.Cells("id").Value), CStr(salida.Cells("Factura").Value), CDec(salida.Cells("chmMontoPago").Value)))
+                listaSalidas.Add(New Tuple(Of Integer, String, Decimal)(CInt(salida.Cells("id").Value), CStr(salida.Cells("Factura").Value), CDec(salida.Cells("txmMontoPagar").Value)))
             Next
 
             mdlPublicVars.superSearchLista3 = listaSalidas
@@ -124,6 +124,7 @@ Public Class frmFacturasElegir
                 ElseIf idcliente > 0 Then
 
                     dt = EntitiToDataTable(conexion.sp_ConsultafacturasVentas(idcliente))
+                    Me.grdFacturas.DataSource = dt
 
                     ' Dim salidas As List(Of tblSalida) = (From x In conexion.tblSalidas Where x.idCliente = idcliente Where x.saldo > 0 And (x.facturado = True Or x.despachar = True Or x.empacado = True) And x.anulado = False Select x Order By x.fechaRegistro Descending).ToList
 
@@ -132,7 +133,7 @@ Public Class frmFacturasElegir
                     'Me.grdFacturas.Rows.Add({elegir, salida.idSalida, CStr(salida.documento), salida.saldo, 0, salida.fechaRegistro.ToShortDateString})
                     'Next
 
-                    Me.grdFacturas.DataSource = dt
+
                     conn.Close()
 
                 End If
@@ -150,9 +151,12 @@ Public Class frmFacturasElegir
     Public Sub fnConfiguracion()
         Try
             Me.grdFacturas.Columns("chmElegir").Width = 75
+            Me.grdFacturas.Columns("chmElegir").ReadOnly = True
             Me.grdFacturas.Columns("Saldo").Width = 75
             Me.grdFacturas.Columns("Factura").Width = 75
+            Me.grdFacturas.Columns("Factura").ReadOnly = True
             Me.grdFacturas.Columns("Fecha").Width = 75
+            Me.grdFacturas.Columns("chmElegir").ReadOnly = True
         Catch ex As Exception
 
         End Try
@@ -166,17 +170,17 @@ Public Class frmFacturasElegir
             Dim fila As Integer = mdlPublicVars.fnGrid_codigoFilaSeleccionada(Me.grdFacturas)
 
             Dim saldo As Decimal = CDec(Me.grdFacturas.Rows(fila).Cells("Saldo").Value)
-            Dim monto As Decimal = CDec(Me.grdFacturas.Rows(fila).Cells("chmMontoPago").Value)
+            Dim monto As Decimal = CDec(Me.grdFacturas.Rows(fila).Cells("txmMontoPagar").Value)
 
             If monto > saldo Then
                 alerta.contenido = "El monto ingresado es mayor al saldo del documento!"
                 alerta.fnErrorContenido()
 
-                Me.grdFacturas.Rows(fila).Cells("chmMontoPago").Value = 0
+                Me.grdFacturas.Rows(fila).Cells("txmMontoPagar").Value = 0
             End If
 
             For i As Integer = 0 To Me.grdFacturas.Rows.Count - 1
-                If CInt(Me.grdFacturas.Rows(i).Cells("chmMontoPago").Value) > 0 Then
+                If CInt(Me.grdFacturas.Rows(i).Cells("txmMontoPagar").Value) > 0 Then
                     Me.grdFacturas.Rows(i).Cells("chmElegir").Value = True
                 End If
             Next
@@ -192,7 +196,7 @@ Public Class frmFacturasElegir
             Dim acreditacion As Decimal = CDec(Me.txtAcreditacionTotal.Text)
 
             For Index As Integer = 0 To Me.grdFacturas.Rows.Count - 1
-                acreditacion -= CDec(Me.grdFacturas.Rows(Index).Cells("chmMontoPago").Value)
+                acreditacion -= CDec(Me.grdFacturas.Rows(Index).Cells("txmMontoPagar").Value)
             Next
 
             Me.txtAcreditacionPendiente.Text = CStr(acreditacion)
